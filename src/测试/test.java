70b39076cc04de92c029aca0287d47e5d3e11974
Arkxd <74634345+Arkxd@users.noreply.github.com>
2023-05-10 10:20:18 +0800

    Initial commit

---
+++ src/测试/test.java
@@ -0,0 +1,14 @@
+package 测试;
+
+public class test {
+    public int do_work(int A, int B) {
+        int x = 0;
+        if ((A > 4) && (B < 9)) {
+            x = A - B;
+        }
+        if ( A == 5 && B > 28) {
+            x = A + B;
+        }
+        return x;
+    }
+}
